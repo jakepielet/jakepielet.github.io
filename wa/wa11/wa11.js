2a005b24da72c8f47ead8145e37d7bf0d8c4fabc
jakepielet <123127415+jakepielet@users.noreply.github.com>
2023-04-09 22:55:56 -0400

    j

---
+++ wa/wa11/wa11.js
@@ -0,0 +1,21 @@
+const newQuoteButton = document.querySelector('#js-new-quote');
+const quoteElement = document.querySelector('#js-quote-text');
+const apiUrl = 'https://opentdb.com/api.php?amount=1&category=21&difficulty=easy&type=boolean';
+
+async function getQuote() {
+    try {
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+    throw new Error('Network response was not ok');
+    }
+    const question = await response.json();
+    console.log(question.results [0].question);
+    quoteElement.innerHTML = question.results [0].question;
+    } catch (error) {
+    console.error('Error fetching quote:', error);
+    alert('Error fetching quote. Please try again later.');
+    }
+}
+
+newQuoteButton.addEventListener('click', getQuote);
+getQuote(); // shows the quote
